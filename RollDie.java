cacf285e5fb0a2bd8edb80027e5a89bef3021688
suresh Daneti <sureshdaneti99@gmail.com>
2021-09-23 11:01:26 +0530

    Player rolls the die to get a number

---
+++ RollDie.java
@@ -0,0 +1,15 @@
+package Com.Bridgelabz;
+
+public class RollDie {
+	public static void main(String[] args) {
+		System.out.println("        Welcome to the Snake & Ladder Game      ");
+
+		int Player_Position=0;
+		System.out.println("\nThe Position of the player at starting is "+Player_Position);
+		int dicenum=(int)(Math.floor(Math.random() * 6 % 7 +1));
+		System.out.println("\nThe Number got while rolling dice "+dicenum);
+	}
+}
+
+
+
